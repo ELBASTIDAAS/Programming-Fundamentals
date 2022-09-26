779b3f8cc4460169887be12512e7e46b2b27527d
Miguel Antonio Bastidas Verdin <miguel.bastidas18@tectijuana.edu.mx>
2022-09-25 21:21:41 -0700

    JS script

---
+++ jsScript.js
@@ -0,0 +1,12 @@
+const tax = 12;
+let UserName="Miguel Antonio"
+let age = 23;
+
+
+
+//console.log("My name is: "+ userName + " and my age is: "+age);
+//console.log("the constant values is "+ tax);
+
+document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
+
+document.write(`<p>Name: ${UserName} </p><p>age is: ${age}</p>`)
