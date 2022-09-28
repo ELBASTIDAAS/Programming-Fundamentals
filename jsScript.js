0684e863bdbfa277603a785be77f88778854814d
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-09-27 17:22:38 -0700

    Update jsScript.js

--- jsScript.js
+++ jsScript.js
@@ -10,3 +10,13 @@ let age = 23;
 document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
 
 document.write(`<p>Name: ${UserName} </p><p>age is: ${age}</p>`)
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+let video = './video.mp4'
+let author = 'BastidasV'
+
+console.log(a + b)
