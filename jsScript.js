b28c4ecc83992b4e16cd45e2bb1f5bbcd9b7ae0c
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-09-27 20:11:24 -0700

    Update jsScript.js

--- jsScript.js
+++ jsScript.js
@@ -20,35 +20,20 @@ let especial = '@'
 let more_especial = '%';
 
 
-
-
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
 
 //document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
 
 document.write(`<p>Name: ${UserName} </p><p>age is: ${agee}</p><p> Email: ${email}</p><p>Title: ${title}</p><p>Total movies: ${movies}</p><p>${buscar}</p><p>Your number is: ${phone}</p><p>Dolar price: ${dolar}</p><p>Animal: ${animal}</p><p>Character: ${x}</p><p>this is a double: ${r}</p><p>TAX: ${tax}</p><p>FAX: ${fax}</p><p>My son: ${son}</p><p>More character: ${morechar}</p><p>Flag: ${flag}</p><p>Red flag: ${redflag}</p><p>Valor: ${row}</p><p>Especial Character: ${especial}</p><p>More especial character: ${more_especial}</p>`)
 
+//----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
+let studentName = prompt("Enter student name");; //string, "",'',``
+let studentAge = prompt("Enter student age");; //number 1,0000000, 1.3, any number
+let gpa = prompt("Enter GPA");; //boolean
 
-
-//let a = 1;
-//let b = 2;
-//let c = 3;
-
-//function testSum(){
-    //let sum = a + b;
-    //{
-      //  let sum = a + c;
-        
-    //}
-    //return sum
-//}
-
-console.log("This is the console")
-var age = 10;
-
-let studentName = "Frank"; //string, "",'',``
-let studentAge = 20; //number 1,0000000, 1.3, any number
-let isActivate = true; //boolean
-
+let newGPA = .3 + Number(gpa);
 document.write("<p> Name: " + studentName + "</p>"); //concatenation
 document.write("<p> Age: " + studentAge + "</p>");
-document.write("<p> Activate: " + isActivate + "</p>");
+document.write("<p> GPA: " + newGPA + "</p>");
