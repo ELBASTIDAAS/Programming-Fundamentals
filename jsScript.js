fa9d6c5725fd2a9f1bf7947640389c8ae4065be9
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-09-27 19:17:03 -0700

    concatenation

--- jsScript.js
+++ jsScript.js
@@ -1,46 +1,49 @@
-const tax = 12;
-let UserName="Miguel Antonio"
-let age = 23;
-let email = "miguel.bastidas18@tectijuana.edu.mx"
-let title = "Netflix"
-let movies = 50;
-let buscar = "search"
-let phone = 6647238264;
-let dolar = 19;
-let animal = "Panda"
-let x = 'a'
-let r = 13.5;
-let fax = 982361723;
-let son = "daniela"
-let morechar = 'R'
+//const tax = 12;
+//let UserName="Miguel Antonio"
+//let age = 23;
+//let email = "miguel.bastidas18@tectijuana.edu.mx"
+//let title = "Netflix"
+//let movies = 50;
+//let buscar = "search"
+//let phone = 6647238264;
+//let dolar = 19;
+//let animal = "Panda"
+//let x = 'a'
+//let r = 13.5;
+//let fax = 982361723;
+//let son = "daniela"
+//let morechar = 'R'
 
 
 
-//console.log("My name is: "+ userName + " and my age is: "+age);
-//console.log("the constant values is "+ tax);
-
-document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
-
-document.write(`<p>Name: ${UserName} </p><p>age is: ${age}</p><p> Email: ${email}</p><p>Title: ${title}</p><p>Total movies: ${movies}</p><p>${buscar}</p><p>Your number is: ${phone}</p><p>Dolar price: ${dolar}</p><p>Animal: ${animal}</p><p>Character: ${x}</p><p>this is a double: ${r}</p><p>TAX: ${tax}</p><p>FAX: ${fax}</p><p>My son: ${son}</p><p>More character: ${morechar}</p>`)
-
-//let video = './video.mp4'
-//let author = 'BastidasV'
-
-//console.log(a + b)
-
-//let name = 'Mike'
-//document.write(``)
 
 
-let a = 1;
-let b = 2;
-let c = 3;
+//document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
 
-function testSum(){
-    let sum = a + b;
-    {
-        let sum = a + c;
+//document.write(`<p>Name: ${UserName} </p><p>age is: ${age}</p><p> Email: ${email}</p><p>Title: ${title}</p><p>Total movies: ${movies}</p><p>${buscar}</p><p>Your number is: ${phone}</p><p>Dolar price: ${dolar}</p><p>Animal: ${animal}</p><p>Character: ${x}</p><p>this is a double: ${r}</p><p>TAX: ${tax}</p><p>FAX: ${fax}</p><p>My son: ${son}</p><p>More character: ${morechar}</p>`)
+
+
+
+//let a = 1;
+//let b = 2;
+//let c = 3;
+
+//function testSum(){
+    //let sum = a + b;
+    //{
+      //  let sum = a + c;
         
-    }
-    return sum
-}
+    //}
+    //return sum
+//}
+
+console.log("This is the console")
+var age = 10;
+
+let studentName = "Frank"; //string, "",'',``
+let studentAge = 20; //number 1,0000000, 1.3, any number
+let isActivate = true; //boolean
+
+document.write("<p> Name: " + studentName + "</p>"); //concatenation
+document.write("<p> Age: " + studentAge + "</p>");
+document.write("<p> Activate: " + isActivate + "</p>");
