fb261949c8651493e256bd5dbfcfe9f2a8cf20bf
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-09-27 19:27:06 -0700

    20 variables

--- jsScript.js
+++ jsScript.js
@@ -1,18 +1,23 @@
-//const tax = 12;
-//let UserName="Miguel Antonio"
-//let age = 23;
-//let email = "miguel.bastidas18@tectijuana.edu.mx"
-//let title = "Netflix"
-//let movies = 50;
-//let buscar = "search"
-//let phone = 6647238264;
-//let dolar = 19;
-//let animal = "Panda"
-//let x = 'a'
-//let r = 13.5;
-//let fax = 982361723;
-//let son = "daniela"
-//let morechar = 'R'
+const tax = 12;
+let UserName="Miguel Antonio"
+let agee = 23;
+let email = "miguel.bastidas18@tectijuana.edu.mx"
+let title = "Netflix"
+let movies = 50;
+let buscar = "search"
+let phone = 6647238264;
+let dolar = 19;
+let animal = "Panda"
+let x = 'a'
+let r = 13.5;
+let fax = 982361723;
+let son = "daniela"
+let morechar = 'R'
+let flag = true;
+let redflag = false;
+let row = null;
+let especial = '@'
+let more_especial = '%';
 
 
 
@@ -20,7 +25,7 @@
 
 //document.write("<p> Name: "+UserName +" and the age is: "+age+ "</p>");
 
-//document.write(`<p>Name: ${UserName} </p><p>age is: ${age}</p><p> Email: ${email}</p><p>Title: ${title}</p><p>Total movies: ${movies}</p><p>${buscar}</p><p>Your number is: ${phone}</p><p>Dolar price: ${dolar}</p><p>Animal: ${animal}</p><p>Character: ${x}</p><p>this is a double: ${r}</p><p>TAX: ${tax}</p><p>FAX: ${fax}</p><p>My son: ${son}</p><p>More character: ${morechar}</p>`)
+document.write(`<p>Name: ${UserName} </p><p>age is: ${agee}</p><p> Email: ${email}</p><p>Title: ${title}</p><p>Total movies: ${movies}</p><p>${buscar}</p><p>Your number is: ${phone}</p><p>Dolar price: ${dolar}</p><p>Animal: ${animal}</p><p>Character: ${x}</p><p>this is a double: ${r}</p><p>TAX: ${tax}</p><p>FAX: ${fax}</p><p>My son: ${son}</p><p>More character: ${morechar}</p><p>Flag: ${flag}</p><p>Red flag: ${redflag}</p><p>Valor: ${row}</p><p>Especial Character: ${especial}</p><p>More especial character: ${more_especial}</p>`)
 
 
 
